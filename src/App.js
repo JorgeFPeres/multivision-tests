c207e16460d38c226999dd7c2a8227a4ea20ecf3
Jorge Peres <jorge-peres@live.com>
2021-05-08 14:49:44 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
 function App() {
+  const [number, setNumber] = useState('')
+  const [hasNumber, setHasNumber] = useState(false)
+  const [prime, setPrime] = useState(false)
+
+  function numeroPrime(num) {
+    for (let divisor = 2; divisor < num; divisor++) {
+      if (num % divisor === 0) {
+        return false
+      }
+    }
+    return true
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setPrime(numeroPrime(number))
+    setHasNumber(true)
+    setNumber('')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <div className='glass'>
+        <div className='left-container'>
+          <h2>Insira um número primo</h2>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <input
+                type='text'
+                value={number}
+                className='input-bar'
+                onChange={(e) => setNumber(e.target.value)}
+              />
+            </div>
+            <button className='btn' type='submit'>
+              Verificar
+            </button>
+          </form>
+        </div>
+        <div className='results'>
+          <h3> Resultado: </h3>
+          <p>
+            {hasNumber ? (prime ? `É primo ` : 'Não é um número primo') : '...'}
+          </p>
+        </div>
+      </div>
+      <div className='circle1'></div>
+      <div className='circle2'></div>
+    </main>
+  )
 }
 
-export default App;
+export default App
